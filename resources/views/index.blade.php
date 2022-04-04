aa682fafa6098d0dec1ab36e8646401dfa246eeb
botwilly13 <william.degh@gmail.com>
2022-04-04 15:52:50 +0200

    upp

---
+++ resources/views/index.blade.php
@@ -0,0 +1,46 @@
+<x-guest-layout>
+    
+    <!DOCTYPE html>
+    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    </head>
+
+    <body>
+
+        <section class="text-center my-5">
+            <h1 class="mb-3 text-white">Bienvenu chez O'Malley</h1>
+
+            @Auth
+            <a href="{{ route('login') }}"><button type="button" class="border rounded px-3 py-2 text-white">Accéder à mon espace</button></a>
+            @else
+            <a href="{{ route('login') }}"><button type="button" class="border rounded px-3 py-2 text-white">Je me connecte</button></a>
+            @endif
+
+
+        </section>
+        
+    </body>
+
+    </html>
+
+</x-guest-layout>
+
+<style scoped>
+    body {
+        background-color: #0e131c;
+        margin: 0;
+        padding: 0;
+    }
+
+</style>
---
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -0,0 +1,6 @@
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+    <div style="border: 2px solid #282f3d;background-color: #191f2b"
+        class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        {{ $slot }}
+    </div>
+</div>
