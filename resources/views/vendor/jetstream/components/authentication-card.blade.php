4f8b4646b2cd75feee118aee05008c52444a72ef
botwilly13 <71338748+botwilly13@users.noreply.github.com>
2022-04-04 19:47:05 +0200

    Update authentication-card.blade.php

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,6 +1,5 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-    <div style="border: 2px solid #282f3d;background-color: #191f2b"
-        class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0" style="border: 2px solid #282f3d;background-color: #0e131c">
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg" style="border: 2px solid #282f3d; background-color: #212733">
         {{ $slot }}
     </div>
 </div>
