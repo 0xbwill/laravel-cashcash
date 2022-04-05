29e9445e6df79033f62024699e3865d68d5eeeca
botwilly13 <william.degh@gmail.com>
2022-04-05 09:22:08 +0200

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,15 +16,15 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('surname');
+            $table->string('surname')->nullable();
             $table->boolean('isTechnicien')->nullable();
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('qualification');
-            $table->string('adresse');
-            $table->date('dateObtention');
-            $table->date('dateEmbauche');
-            $table->integer('numeroAgence');
+            $table->string('qualification')->nullable();
+            $table->string('adresse')->nullable();
+            $table->date('dateObtention')->nullable();
+            $table->date('dateEmbauche')->nullable();
+            $table->integer('numeroAgence')->nullable();
             $table->rememberToken();
             $table->timestamp('email_verified_at')->nullable();
             $table->foreignId('current_team_id')->nullable();
