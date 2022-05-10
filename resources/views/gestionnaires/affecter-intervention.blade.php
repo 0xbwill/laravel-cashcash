1cc05f986afc5cb1fdbb5ce983c64f0c4ee4170a
NicoVnd <77612888+NicoVnd@users.noreply.github.com>
2022-05-10 22:25:16 +0200

    commentaire

--- resources/views/gestionnaires/affecter-intervention.blade.php
+++ resources/views/gestionnaires/affecter-intervention.blade.php
@@ -4,8 +4,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="overflow-hidden sm:rounded-lg text-white">
                 <div class="text-center mb-2">
+                {{-- Route ramenant vers le page Dashboard (dashboard.blade.php) --}}
                     <a href="{{ route('dashboard') }}">Changer d'agence</a>
                 </div>
+                 {{-- Affiche l'agence selectionnée --}}
                 <div class="d-flex justify-center">
                     <h4 class="h4 text-center">Agence selectionnée : <br>
                         @foreach ($agence as $uneAgence)
@@ -20,12 +22,14 @@
                     <div class="row d-flex justify-center">
                         <div class="col-md-6 mx-5 rounded-lg h-100 pb-3"
                             style="border: 2px #282f3d solid;background-color: #0e131c">
+                            {{-- Affiche la liste des clients correspondant à l'agence selectionnée  --}}
                             <h5 class="h5 text-center my-4">Liste des clients :</h5>
                             @foreach ($listeClient as $unClient)
                                 <div class="d-flex justify-content-between mt-3">
                                     <p class="align-self-center ">
                                         {{ $unClient->numeroAgence . ' | ' . $unClient->nom . ' ' . $unClient->prenom }}
                                     </p>
+                                    {{-- Liens vers la page permettant d'assigner une nouvelle intervetion (gestionnaires/new-intervention.blade.php)  --}}
                                     <a href="{{ url('assigner/intervention/' . $unClient->numeroClient) }}"
                                         class="btn btn-primary">Assigner une intervention</a>
                                 </div>
--- resources/views/gestionnaires/new-intervention.blade.php
+++ resources/views/gestionnaires/new-intervention.blade.php
@@ -8,28 +8,30 @@
                     @csrf
                     <label for="numeroIntervenant">Intervenant :</label>
                     <select class="form-control mb-4" name="numeroIntervenant" id="numeroIntervenant">
+                        {{-- Liste déroulante pour selectionner un intervenant de la même agence que les clients --}}
                         <option value="">Sélectionnez un intervenant</option>
                         @foreach ($intervenants as $unIntervant)
                             <option value="{{ $unIntervant->id }}">
                                 {{ $unIntervant->id . ' | ' . $unIntervant->name . ' ' . $unIntervant->surname }}</option>
                         @endforeach
                     </select>
-
+                        {{-- Permet de selectionner une date --}}
                         <label for="dateVisite">Date visite :</label>
                         <input class="form-control mb-4" type="date" name="dateVisite" id="dateVisite">
-
+                        {{-- Permet de selectionner une heure --}}
                         <label for="heureVisite">Heure visite :</label>
                         <input class="form-control mb-4" type="time" name="heureVisite" id="heureVisite">
                    
                     <input type="hidden" name="tel" id="tel" value="{{ $newIntervention->tel }}">
 
-
+                    {{-- Bouton permettant de confirmer l'assignation d'une nouvelle interventions  --}}
                     <button class="btn btn-primary mt-3" type="submit">Confirmer</button>
                     <input name="numeroClient" type="hidden" value="{{ $newIntervention->numeroClient }}">
                 </form>
             </div>
             <div class="col-md-5 mx-5 rounded-lg h-100 pb-3 text-white px-4 pb-4"
                 style="border: 2px #282f3d solid;background-color: #0e131c">
+                {{-- Affiche des informations du client à droite sur la page --}}
                 <h5 class="h5 text-center my-4">Informations client</h5>
                 <p>Numéro client : {{ $newIntervention->numeroClient }}</p>
                 <p>Nom & prénom : {{ $newIntervention->nom . ' ' . $newIntervention->prenom }}</p>
